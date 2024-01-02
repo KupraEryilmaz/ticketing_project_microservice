377859c8e4aa61d32b709f659c7c7760c199d052
KupraEryilmaz <kupraeryilmaz@gmail.com>
2024-01-03 01:31:27 +0300

    eureka

---
+++ discovery-service/src/test/java/com/cydeo/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
